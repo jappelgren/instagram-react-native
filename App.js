04e4680b177ca07edd89fbbb6137064f8069b485
Justin Appelgren <justinappelgren@gmail.com>
2021-05-12 15:40:19 -0500

    styled views so navbar is stuck to footer area

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import NavBar from './src/screens/NavBar/NavBar.jsx';
 
 export default function App() {
+  const styles = StyleSheet.create({
+    mainContainer: {
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+
+    },
+    mainView: {
+      flexGrow: 12,
+    }
+
+  });
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.mainContainer}>
       <StatusBar style="auto" />
+      {/* vv placeholder vv */}
+      <ScrollView style={styles.mainView}>
+        <Text>Hi</Text>
+      </ScrollView>
+      {/* ^^ placeholder ^^ */}
       <NavBar />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/NavBar/NavBar.jsx
+++ src/screens/NavBar/NavBar.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { View } from 'react-native'
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 
 export default function NavBar() {
-    return (
-        <View>
-            <Image
-            source= {{
-                uri: "assets/iconmonstr-home-6.svg",
-            }}
-            />
-        </View>
-    )
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width: '100%',
+      border: 'solid',
+      justifyContent: 'center',
+      alignContent: 'space-between'
+    },
+    icon: {
+      width: 25,
+      height: 25,
+    },
+  });
+  return (
+    <View style={styles.container}>
+      <Image style={styles.icon} source={require('./iconmonstr-home-6.svg')} />
+    </View>
+  );
 }
