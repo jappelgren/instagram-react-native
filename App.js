62a9b54d36b389f627fbac71608be7387ad63635
Simeon Willard <swillard4597@gmail.com>
2021-05-12 13:53:42 -0500

    created nav

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NavBar from './src/screens/NavBar/NavBar.jsx';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
+      <NavBar />
     </View>
   );
 }
---
+++ src/screens/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default function NavBar() {
+    return (
+        <View>
+            <Image
+            source= {{
+                uri: "assets/iconmonstr-home-6.svg",
+            }}
+            />
+        </View>
+    )
+}
