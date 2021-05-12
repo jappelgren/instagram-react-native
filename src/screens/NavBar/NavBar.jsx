9357d9266b28139d34f4770a6a63a1d1a0fd1e2c
Justin Appelgren <justinappelgren@gmail.com>
2021-05-12 15:50:01 -0500

    Added all icons to navbar

--- src/screens/NavBar/NavBar.jsx
+++ src/screens/NavBar/NavBar.jsx
@@ -5,10 +5,10 @@ export default function NavBar() {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
+      flexDirection: 'row',
       width: '100%',
-      border: 'solid',
-      justifyContent: 'center',
-      alignContent: 'space-between'
+      justifyContent: 'space-around',
+      alignItems: 'center',
     },
     icon: {
       width: 25,
@@ -18,6 +18,21 @@ export default function NavBar() {
   return (
     <View style={styles.container}>
       <Image style={styles.icon} source={require('./iconmonstr-home-6.svg')} />
+      <Image
+        style={styles.icon}
+        source={require('./iconmonstr-magnifier-2.svg')}
+      />
+      <Image
+        style={styles.icon}
+        source={require('./iconmonstr-photo-camera-4.svg')}
+      />
+      <Image
+        style={styles.icon}
+        source={require('./iconmonstr-favorite-2.svg')}
+      />
+      {/* vv Placeholder for profile pic of user vv */}
+      <Image style={styles.icon} source={require('./iconmonstr-user-19.svg')} />
+      {/* ^^ Placeholder for profile pic of user ^^ */}
     </View>
   );
 }
