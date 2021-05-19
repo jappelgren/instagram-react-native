03b719f928b64ec52118a2bba5572df38b2821ab
Simeon Willard <swillard4597@gmail.com>
2021-05-19 15:20:37 -0500

    resolved conflicts

---
+++ src/screens/Caption/Caption.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+
+
+export default function Caption() {
+    return (
+        <View>
+            <Text>Write a Caption</Text>
+        </View>
+    )
+}
