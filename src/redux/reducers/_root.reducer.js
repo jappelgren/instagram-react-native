41186ad4cc20077df2e2fa74ee8e92b3d6691ae3
Justin Appelgren <justinappelgren@gmail.com>
2021-05-20 13:04:42 -0500

    added user and error reducers

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
-
+import user from './user.reducer';
+import error from './error.reducer'
 
 
 const rootReducer = combineReducers({
-    
-})
+    user,
+    error
+});
 
 export default rootReducer;
