ecd2879b75930405a5396fae9d03baa1a43873dc
Simeon Willard <swillard4597@gmail.com>
2021-05-21 14:37:56 -0500

    created posts server route and saga

--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -2,9 +2,23 @@ const express = require('express');
 const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
-const userStrategy = require('../strategies/user.strategy');
-
 const router = express.Router();
 
+router.post('/', rejectUnauthenticated, (req, res) => {
+    const newPost = req.body.newPost;
+
+    const sqlQuery = `INSERT INTO "posts" ("image", "description", "user_id")
+                      VALUES ($1, $2, $3);`
+
+    pool.query(sqlQuery, [newPost.url, newPost.description, req.user.id])
+    .then((result) => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log('error adding post to db', error);
+        res.sendStatus(500);
+    })
+})
+
+module.exports = router;
