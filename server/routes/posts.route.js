d93b8c2991962fdc3ecdb37648dd6bc6d0a5f400
Simeon Willard <swillard4597@gmail.com>
2021-05-21 15:34:29 -0500

    resolved merge

--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -5,14 +5,13 @@ const {
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
     const newPost = req.body.newPost;
-    console.log(newPost)
 
-    const sqlQuery = `INSERT INTO "posts" ("image", "description")
-                      VALUES ($1, $2);`
+    const sqlQuery = `INSERT INTO "posts" ("image", "description", "user_id")
+                      VALUES ($1, $2, $3);`
 
-    pool.query(sqlQuery, [newPost.url, newPost.description])
+    pool.query(sqlQuery, [newPost.url, newPost.description, req.user.id])
     .then((result) => {
         res.sendStatus(200);
     })
